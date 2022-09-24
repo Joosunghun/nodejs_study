22bf15c897f3080e822978f798c8961467339fee
BewB <86991902+Joosunghun@users.noreply.github.com>
2022-09-24 16:48:34 +0900

    Create 2_1_7_1.js
    프로미스

---
+++ ch02/2_1_7_1.js
@@ -0,0 +1,39 @@
+const condition = true;
+const promise = new Promise((resolve, reject) => {
+    if(condition){
+        resolve('성공');
+    }
+    else{
+        reject('실패');
+    }
+});
+// 다른 코드가 들어갈 수 있음
+promise
+    .then((message) => {
+        console.log(message); // 성공(resolve)한 경우 실행
+    })
+    .catch((error) => {
+        console.error(error); // 실패(reject)한 경우 실행
+    })
+    .finally(() => { // 끝나고 무조건 실행
+        console.log('무조건'); 
+    })
+
+promise
+    .then((message) =>{
+        return new Promise((resolve, reject) => {
+            resolve(message);
+        });
+    })
+    .then((message2) => {
+        console.log(message2);
+        return new Promise((resolve, reject) =>{
+            resolve(message2);
+        });
+    })    
+    .then((message3) => {
+        console.log(message3);
+    })
+    .catch((error) => {
+        console.log(error);
+    });
